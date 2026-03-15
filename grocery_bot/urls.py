c7af242685e1efc341b2e38cb40765eda2a91961
DivyansuVerma <130806575+Divyansuverma@users.noreply.github.com>
2026-03-16 00:57:19 +0530

    fix all issuess

--- grocery_bot/urls.py
+++ grocery_bot/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from bot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webhook/', views.webhook, name='webhook'),
+    re_path(r'^webhook/?$', views.webhook, name='webhook'),
     path("menu/", views.web_menu, name="web_menu"),
     path("api/web-order/", views.web_order),
     path("api/add-to-cart/", views.web_add_to_cart),
